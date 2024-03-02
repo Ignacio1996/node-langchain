6bcd8973649c270beb7ff5aa52542c2095dfff25
Nicolas Aguirre <inaguirre96@gmail.com>
2024-03-02 12:16:32 -0500

    basic query route se

--- server.js
+++ server.js
@@ -1,13 +1,25 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 
-app.post("/queryFiles", (req, res) => {
-  const data = req.body;
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-  res.json({
-    received: "Hello!",
-  });
+app.post("/queryFiles", async (req, res) => {
+  const { query } = req.body;
+
+  console.log("server.js 7 | body", query, !query);
+
+  if (query) {
+    res.json({
+      received: "Hello!",
+    });
+  } else {
+    res.json({
+      error: "no data",
+    });
+  }
 });
 
 app.listen(process.env.PORT || "8080", () => {
